d952d0e7e8664f1efe71e175dbb9ce5a087f41b9
KANTE MOUSSA <service.gratos.sg>
2024-07-25 15:04:16 +0000

    premier commit

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -8,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './intro.component.scss'
 })
 export class IntroComponent {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  snaped!: boolean;
+  snapedtext!:string;
 
+  ngOnInit() {
+    this.title = 'KOBA';
+    this.description = 'KOBA est une entreprise ivoirienne reconnue pour son expertise';
+    this.createdAt = new Date();
+    this.snaps = 5;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snaped= false;
+    this.snapedtext="🤍 j aime pas"
+  }
+  onAddSnap() {
+    if(this.snaped === false){
+      this.snaps++;
+      this.snapedtext="❤️ j aime"
+       this.snaped=true
+    }
+    else
+    {
+      this.snaps--
+      this.snapedtext="🤍 j aime pas"
+      this.snaped=false
+    }
+
+    
+
+  }
+
+  
 }
